8128ff07c22649196b84c588bf3fc9c0f098ac54
Martins Ozolins <ozolins.martinsh@gmail.com>
2025-11-19 20:56:36 +0000

    feat: add new graph solution

---
+++ graphs/1926_nearest_exit_from_entrance_in_maze/1926_nearest_exit_from_entrance_in_maze.py
@@ -0,0 +1,44 @@
+from collections import deque
+from typing import List
+
+
+class Solution:
+    def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
+        n, m = len(maze), len(maze[0])
+        sr, sc = entrance
+        visited = set()
+        visited.add((sr, sc))  # mark entrance as visited
+
+        q = deque([(sr, sc, 0)])  # (row, col, distance)
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]  # 4 movement directions
+
+        while q:
+            # pop position to explore from
+            row, column, distance = q.popleft()
+
+            # explore all four directions (move by ±1)
+            for dr, dc in directions:
+                next_row, next_col = row + dr, column + dc
+
+                # valid cell, not visited, and open path
+                if (
+                    0 <= next_row < n
+                    and 0 <= next_col < m
+                    and maze[next_row][next_col] == "."
+                    and (next_row, next_col) not in visited
+                ):
+
+                    # if boundary cell, return distance + 1
+                    if (
+                        next_row == 0
+                        or next_row == n - 1
+                        or next_col == 0
+                        or next_col == m - 1
+                    ):
+                        return distance + 1
+
+                    # otherwise continue BFS
+                    visited.add((next_row, next_col))
+                    q.append((next_row, next_col, distance + 1))
+
+        return -1  # no exit found
